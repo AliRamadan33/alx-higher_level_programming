11fd42109c36f7c0116db50ac7794f93ba751260
LinCu0 <alitoba335@gmail.com>
2023-06-05 10:17:42 -0700

    new

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str.split(" ")[5][:-1], str.split(" ")[6], str.split(" ")[7], str.split(" ")[8], str.split(" ")[9], "with Python")
