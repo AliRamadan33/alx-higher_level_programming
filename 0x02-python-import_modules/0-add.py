f7cfee1cd59de8d50e074cff3c671c9792dca44f
LinCu0 <alitoba335@.com>
2023-06-12 14:31:36 -0700

    LO

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3"""
+from add_0 import add
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
