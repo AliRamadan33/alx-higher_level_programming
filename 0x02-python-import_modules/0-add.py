5d9c66d679b50b0f7d1bbdbbe7566422a5d7b234
LinCu0 <alitoba335@.com>
2023-06-12 14:32:56 -0700

    LO

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3"""
 from add_0 import add
 
