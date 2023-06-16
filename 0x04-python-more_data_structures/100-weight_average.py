ed78258e82365e7303bd1582298ce31eef922aa0
LinCu0 <alitoba335@.com>
2023-06-16 07:07:42 -0700

    More..:

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
+def weight_average(my_list=[]):
+    if not my_list:
         return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    result = 0
-    for i in range(len(roman_string)):
-        if i > 0 and roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]:
-            result += roman_dict[roman_string[i]] - 2 * roman_dict[roman_string[i - 1]]
-        else:
-            result += roman_dict[roman_string[i]]
-    return result
+    numerator = sum([x[0] * x[1] for x in my_list])
+    denominator = sum([x[1] for x in my_list])
+    return numerator / denominator
