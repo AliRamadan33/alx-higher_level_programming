1c996c951ee8f3938db52c5f68dfd916c6cce196
LinCu0 <alitoba335@gmail.com>
2023-07-11 14:37:13 -0700

    I/O

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    with open(filename, encoding='utf-8') as f:
-        print(f.read(), end="")
+    with open(filename, "r", encoding="utf8") as file:
+        for line in file:
+            print(line, end="")
