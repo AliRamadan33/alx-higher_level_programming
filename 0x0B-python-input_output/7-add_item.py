48cebff27173840d521e445934c977fe56d52727
LinCu0 <alitoba335@gmail.com>
2023-07-13 00:00:28 +0000

    I/O

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Module that contain a functions that returns an object by."""
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
 if __name__ == "__main__":
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        items = []i
+        items = []
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
