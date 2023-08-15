9ca77ad47a5f636517c80bc5ce8d25773cb448fc
LinCu0 <alitoba335@gmail.com>
2023-08-15 23:15:55 +0300

    MYSQL

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
--- script to list rows with the name values.
+-- Change character SET and COLLATE
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 USE hbtn_0c_0
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
