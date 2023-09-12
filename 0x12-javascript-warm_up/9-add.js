0190209cc905e4bd890d6f6e3d4bb893a094c8e2
LinCu0 <alitoba335@gmail.com>
2023-09-12 00:40:59 +0000

    Js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
-// Prints a message depending on number of arguments passed
-
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 function add (a, b) {
-    return a + b;
-  }
-  
-  const args = process.argv;
-  const num1 = parseInt(args[2], 10);
-  const num2 = parseInt(args[3], 10);
-  
-  console.log(add(num1, num2));
+  return (a + b);
+}
+console.log(add(a, b));
