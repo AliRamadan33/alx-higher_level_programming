f78b4a62e6b882215f75fadf81f8b23683e2d708
LinCu0 <alitoba335@gmail.com>
2023-09-12 00:35:07 +0000

    Js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Prints a message depending on number of arguments passed
+
+const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < langs.length; i++) {
+  console.log(langs[i]);
+}
