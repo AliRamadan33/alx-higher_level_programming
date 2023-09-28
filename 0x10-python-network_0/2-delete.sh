ed36f6b16e01c69f9d84323ac4bcb02a9e9df9ca
LinCu0 <alitoba335@gmail.com>
2023-09-28 07:25:02 -0700

    Net

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# takes in a URL, send a request to that URL, and display the size of the body of the response
+# sends a DELETE request to the URL passed as the first argument and display the body of the response
 curl -sX DELETE $1 -L
-
